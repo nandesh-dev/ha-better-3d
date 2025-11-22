ecd770df0a45db88ec2741d8713ec701980efacc
nandesh-dev <nandesh.dev@gmail.com>
2025-11-22 12:33:40 +0000

    impr: default configuration

--- src/configuration/index.ts
+++ src/configuration/index.ts
@@ -1,5 +1,3 @@
-import { CARD_CUSTOM_ELEMENT_TAGNAME } from '@/elements/card'
-
 import {
     DEFAULT_AMBIENT_LIGHT_CONFIGURATION,
     DEFAULT_CARD_3D_CONFIGURATION,
@@ -12,6 +10,40 @@ import {
 import { Expression, decodeExpression, decodeString, encodeExpression, encodeString } from './value'
 
 const CONFIGURATION_RESERVED_KEYS: string[] = ['type', 'activeScene', 'scenes', 'styles'] as const
+export const CARD_ELEMENT_TAGNAME = process.env.PRODUCTION ? 'better-3d-card' : 'better-3d-card_development'
+
+export type SceneConfiguration = {
+    activeCamera: Expression
+    backgroundColor: Expression
+    objects: ObjectConfigurationMap
+}
+
+export const DEFAULT_SCENE_CONFIGURATION: SceneConfiguration = {
+    activeCamera: '"primary_camera"',
+    backgroundColor: 'new Color("#eeeeee")',
+    objects: {
+        primary_camera: DEFAULT_PERSPECTIVE_CAMERA_CONFIGURATION,
+        point_light: DEFAULT_POINT_LIGHT_CONFIGURATION,
+        ambient_light: DEFAULT_AMBIENT_LIGHT_CONFIGURATION,
+        logo: DEFAULT_CARD_3D_CONFIGURATION,
+    },
+} as const
+
+export function decodeSceneConfiguration(raw: any): SceneConfiguration {
+    return {
+        activeCamera: decodeExpression(raw.active_camera, DEFAULT_SCENE_CONFIGURATION.activeCamera),
+        backgroundColor: decodeExpression(raw.background_color, DEFAULT_SCENE_CONFIGURATION.backgroundColor),
+        objects: decodeObjectMap(raw.objects),
+    }
+}
+
+export function encodeSceneConfiguration(config: SceneConfiguration): unknown {
+    return {
+        active_camera: encodeExpression(config.activeCamera),
+        background_color: encodeExpression(config.backgroundColor),
+        objects: encodeObjectMap(config.objects),
+    }
+}
 
 export type Configuration = {
     type: string
@@ -21,14 +53,33 @@ export type Configuration = {
     extra: { [key: string]: unknown }
 }
 
+export const DEFAULT_CONFIGURATION = encodeConfiguration({
+    type: `custom:${CARD_ELEMENT_TAGNAME}`,
+    activeScene: '"primary_scene"',
+    styles: `.better-3d__card {
+  display: block;
+  position: relative;
+  width: 100%;
+  aspect-ratio: 2/1;
+}`,
+    scenes: {
+        primary_scene: DEFAULT_SCENE_CONFIGURATION,
+    },
+    extra: {},
+})
+
 export function decodeConfiguration(raw: any): Configuration {
+    if (Object.keys(raw).length == 1) {
+        return decodeConfiguration(DEFAULT_CONFIGURATION)
+    }
+
     const scenes: { [name: string]: SceneConfiguration } = {}
     for (const name in raw.scenes || []) {
         scenes[name] = decodeSceneConfiguration(raw.scenes[name])
     }
 
     const configuration: Configuration = {
-        type: decodeString(raw, `custom:${CARD_CUSTOM_ELEMENT_TAGNAME}`),
+        type: decodeString(raw.type, `custom:${CARD_ELEMENT_TAGNAME}`),
         activeScene: decodeExpression(raw.active_scene, "''"),
         styles: decodeString(raw.styles, ''),
         scenes,
@@ -55,38 +106,5 @@ export function encodeConfiguration(config: Configuration): unknown {
         styles: encodeString(config.styles),
         scenes,
         ...config.extra,
-    }
-}
-
-export type SceneConfiguration = {
-    activeCamera: Expression
-    backgroundColor: Expression
-    objects: ObjectConfigurationMap
-}
-
-export const DEFAULT_SCENE_CONFIGURATION: SceneConfiguration = {
-    activeCamera: '"primary_camera"',
-    backgroundColor: 'new Color("#eeeeee")',
-    objects: {
-        primary_camera: DEFAULT_PERSPECTIVE_CAMERA_CONFIGURATION,
-        point_light: DEFAULT_POINT_LIGHT_CONFIGURATION,
-        ambient_light: DEFAULT_AMBIENT_LIGHT_CONFIGURATION,
-        logo: DEFAULT_CARD_3D_CONFIGURATION,
-    },
-} as const
-
-export function decodeSceneConfiguration(raw: any): SceneConfiguration {
-    return {
-        activeCamera: decodeExpression(raw.active_camera, '""'),
-        backgroundColor: decodeExpression(raw.background_color, 'new Color("#eeeeee")'),
-        objects: decodeObjectMap(raw.objects),
-    }
-}
-
-export function encodeSceneConfiguration(config: SceneConfiguration): unknown {
-    return {
-        active_camera: encodeExpression(config.activeCamera),
-        background_color: encodeExpression(config.backgroundColor),
-        objects: encodeObjectMap(config.objects),
     }
 }
